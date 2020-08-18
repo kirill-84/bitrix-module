36ca1fed481124453ec40ae100d7a2db3d3c1045
Kirill <35264591+kirill-84@users.noreply.github.com>
2020-08-18 12:14:45 +0300

    Create lang xyz_geolocation_edit.php

---
+++ lang/ru/admin/xyz_geolocation_edit.php
@@ -0,0 +1,6 @@
+<?
+$MESS["EDIT_CITY_TITLE"] = "Редактирование # #ID#";
+$MESS["EDIT_CITY_NAME"] = "Город - ";
+$MESS["EDIT_CITY_SELECT"] = "Пользователи";
+$MESS["xyz_save_error"] = "Ошибка: ";
+?>
