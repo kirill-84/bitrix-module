ceae699e45ed907e94e11aceba83727cdd192cdf
Kirill <35264591+kirill-84@users.noreply.github.com>
2020-08-18 12:18:15 +0300

    Create lib test.php

---
+++ lib/test.php
@@ -0,0 +1,20 @@
+<?
+
+namespace xyz\geolocation;
+
+use xyz\geolocation\DataTable;
+
+class Test{
+
+    public static function get() {
+        $result = DataTable::getList(
+                        array(
+                            'select' => array('*')
+        ));
+        $row = $result->fetch();
+        print "<pre>"; print_r($row); print "</pre>";
+        return $row;
+    }
+
+}
+?>
