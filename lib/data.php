1234711f83d0e965211eb1606924db4e9bb199bd
Kirill <35264591+kirill-84@users.noreply.github.com>
2020-08-18 12:17:31 +0300

    Create lib data.php

---
+++ lib/data.php
@@ -0,0 +1,62 @@
+<?php
+namespace xyz\geolocation;
+
+use Bitrix\Main\Entity;
+use Bitrix\Main\Localization\Loc;
+ 
+
+/**
+ * Class DataTable
+ * 
+ * Fields:
+ * <ul>
+ * <li> ID int mandatory
+ * <li> LINK string mandatory
+ * <li> PATH string mandatory
+ * <li> SORT int optional default 500
+ * <li> CREATED datetime mandatory default 'CURRENT_TIMESTAMP'
+ * </ul>
+ *
+ * @package \xyz\Data
+ **/
+ 
+class DataTable extends Entity\DataManager
+{
+	/**
+	 * Returns DB table name for entity.
+	 *
+	 * @return string
+	 */
+	public static function getTableName()
+	{
+		return 'CITY';
+	}
+
+	/**
+	 * Returns entity map definition.
+	 *
+	 * @return array
+	 */
+	public static function getMap()
+	{
+		return array(
+			'ID' => array(
+				'data_type' => 'integer',
+				'primary' => true,
+				'autocomplete' => true,
+				'title' => Loc::getMessage('DATA_ENTITY_ID_FIELD'),
+			),
+			'NAME' => array(
+				'data_type' => 'text',
+				'required' => true,
+				'title' => Loc::getMessage('DATA_ENTITY_TITLE_FIELD'),
+			),
+			'CODE' => array(
+				'data_type' => 'text',
+				'required' => true,
+				'title' => Loc::getMessage('DATA_ENTITY_CODE_FIELD'),
+			),
+		);
+	}
+}
+?>
